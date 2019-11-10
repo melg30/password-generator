59b4d686a70f04dae397971c7e016bf1d53d5d59
Mel Garza <melgarza3016@gmail.com>
2019-11-10 13:19:43 -0700

    files and created starting variables

---
+++ script.js
@@ -0,0 +1,5 @@
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split('');
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+var special = "/?~!@#$%^&*()_+-<>".split(``);
+var number = "1234567890".split(``);
+
