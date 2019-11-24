0cbddb5dc16b06ef6c963a3029cd4304dd4eebe8
Mel Garza <melgarza3016@gmail.com>
2019-11-24 13:43:14 -0700

    functioanlly creating and posting a random password

--- script.js
+++ script.js
@@ -1,32 +1,91 @@
-var lowerCase = "abcdefghijklmnopqrstuvwxyz".split('');
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
-var special = "/?~!@#$%^&*()_+-<>".split('');
-var number = "1234567890".split('');
+var lCase = "abcdefghijklmnopqrstuvwxyz".split(""); //split converts a string into an array
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var special = "/?~!@#$%^&*()_+-<>".split("");
+var number = "1234567890".split("");
 
-console.log(lowerCase);
+console.log(lCase);
 console.log(special);
 
 var passwordSpecfics = {
-    confirmUpper: true,
-    confirmLength: 8,
-    confirmSpecial: true,
-    confirmNumber: true,
-}
+  confirmUpper: true,
+  confirmLength: 8,
+  confirmSpecial: true,
+  confirmNumber: true
+};
 
-function specifics(){
+function makePassword(isUpper, isSpecial, isNumber, numLength) {
+  var password = "";
+  // Math.floor(Math.random()*5)  //0 to 4
 
-var confirmUpper = confirm("Do you want your password to contain uppercase letters?");
-var confirmSpecial = confirm("Do you want your password to contain special characters?");
-var confirmNumber = confirm("Do you want your password to contain numbers?");
-var confirmLength = prompt("How long would you like your password? Must be between 8 - 128 characters");
+  // password= password + "P"  //P
+  // password= password + "h" // Ph
+  // pasword=password +"i" //Phi
+  //step 1: get input for length
+  //step 2: iterate # of arrays according to user selection
+  //step 3: staisfy all user conditions
+  //step 4: sting catatination
 
-if (confirmLength < 8 || confirmLength > 128) {
-    alert("Password must be between 8 - 128 characters!")
+  for (i = 0; i < numLength; i++) {
+    {
+      var ranIndexUppercase = Math.floor(Math.random() * upperCase.length);
+      var ranIndexLowercase = Math.floor(Math.random() * lCase.length);
+      var ranIndexSpecial = Math.floor(Math.random() * special.length);
+      var ranIndexNumber = Math.floor(Math.random() * number.length);
+      console.log(
+        ranIndexUppercase,
+        ranIndexLowercase,
+        ranIndexSpecial,
+        ranIndexNumber
+      );
+
+      if (isUpper) {
+        password = password + upperCase[ranIndexUppercase];
+        isUpper = false;
+      } else if (isSpecial) {
+        password = password + special[ranIndexSpecial];
+        isSpecial = false;
+      } else if (isNumber) {
+        password = password + number[ranIndexNumber];
+        isNumber = false;
+      } else {
+        password = password + lCase[ranIndexLowercase];
+      }
+
+      console.log(password);
+    }
   }
+  document.getElementById("password").value = password;
+}
+//makePassword();
 
+function specifics() {
+  var confirmUpper = confirm(
+    "Do you want your password to contain uppercase letters?"
+  );
+  var confirmSpecial = confirm(
+    "Do you want your password to contain special characters?"
+  );
+  var confirmNumber = confirm("Do you want your password to contain numbers?");
+  var confirmLength = prompt(
+    "How long would you like your password? Must be between 8 - 128 characters"
+  );
 
+  if (confirmLength >= 8 && confirmLength <= 128) {
+    confirmLength = parseInt(confirmLength);
+    console.log(confirmUpper, confirmSpecial, confirmNumber, confirmLength);
+
+    makePassword(confirmUpper, confirmSpecial, confirmNumber, confirmLength);
+
+    //if statement is true if all conditions are met
+  } else {
+    alert("Password must be between 8 - 128 characters!"); //error message when false condition
+  }
 }
 
-specifics();
+function clipboard() {
+  alert("Hiiii");
+}
 
-console.log(passwordSpecfics);
+document.getElementById("generate").addEventListener("click", function() {
+  specifics();
+});
